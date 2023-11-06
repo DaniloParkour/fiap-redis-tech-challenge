c0a8345b4e550efff4bd6be420e0ac325c4e51e0
Danilo Carvalho <d.carvalho@ext.ppay.me>
2023-11-06 11:44:59 -0300

    secrets

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "us-east-1"
-  access_key = ""
-  secret_key = ""
+  access_key = "${{ secrets.AWS_ACCESS_KEY_ID }}"
+  secret_key = "${{ secrets.AWS_SECRET_KEY_ID }}"
 }
 
 resource "aws_elasticache_cluster" "redis" {
