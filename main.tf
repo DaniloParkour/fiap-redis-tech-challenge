27c9bb358d62d323060be64c26856498341fb256
Danilo Carvalho <d.carvalho@ext.ppay.me>
2023-11-07 23:26:23 -0300

    redis

--- main.tf
+++ main.tf
@@ -7,8 +7,6 @@ variable "aws_secret_key_id" {
 }
 provider "aws" {
   region = "us-east-1"
-  access_key = var.aws_access_key_id
-  secret_key = var.aws_secret_key_id
 }
 
 resource "aws_elasticache_cluster" "redis" {
