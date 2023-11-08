e8605245db822dccd89d9a8dbdc9014f1ecec56f
Danilo Carvalho <d.carvalho@ext.ppay.me>
2023-11-08 11:09:08 -0300

    logar AWS por secret

--- main.tf
+++ main.tf
@@ -1,14 +1,7 @@
-provider "aws" {
-  region = "us-east-1"
-  access_key = "AKIATU5PCOHSE5TXWM7C"
-  secret_key = "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"
-}
-
 resource "aws_elasticache_cluster" "redis" {
   cluster_id              = "redis-cluster"
   engine                  = "redis"
   node_type               = "cache.t2.micro"
   port                    = "11211"
   num_cache_nodes         = 1
-  #parameter_group_name    = "default.redis.3.2"
 }
